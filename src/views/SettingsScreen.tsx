ff5ad58d4ab289c6705a4531fc86f77c99d16576
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2024-02-02 22:33:49 +0800

    feat: add settings screen

--- src/views/SettingsScreen.tsx
+++ src/views/SettingsScreen.tsx
@@ -5,11 +5,11 @@ import tw from '../styles/tailwind';
 
 import MainLayout from '../components/layouts/MainLayout';
 
-export default function MessageScreen(): JSX.Element {
+export default function SettingsScreen(): JSX.Element {
   return (
     <MainLayout>
       <View style={tw`flex-1 flex-col items-center justify-center`}>
-        <Text style={tw`font-poppins text-white`}>Message Screen</Text>
+        <Text style={tw`font-poppins text-white`}>Settings Screen</Text>
       </View>
     </MainLayout>
   );
